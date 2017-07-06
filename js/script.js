8e6aa54a363fc1e566b6f94158e773927f35fa68
Kim Nguyen <zktnguyen@ucdavis.edu>
2017-07-05 23:38:26 -0700

    Connected all the DOM buttons/interactions through variables. Will continue logic tomorrow

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 (function($) {
-    
+  var toggle = $("#toggle-timer"),
+    timer = $("#clock-timer"),
+    breakTime = $("#break-time"),
+    length = $("#timer-length")
+    decLength = $("#dec-length"),
+    incLength = $("#inc-length"),
+    decBreak = $("#dec-break"),
+    incBreak = $("#inc-break");
+  var timeRemaining = parseFloat(length.html()),
+    minimumLength = 1,
+    breakLength = parseFloat(breakTime.html()),
+    timerInSeconds = timeRemaining * 60,
+    timerType = "S", // "S" = regular timer, "B" = break timer
+    timerOn = false;
+
+  function beginTimer() {
+
+  }
+
+  function stopTimer() {
+
+  }
+
+
+  toggle.on("click",function(e){
+    if (!timerOn){
+      beginTimer();
+    }
+    else {
+      stopTimer();
+    }
+  });
   
+  
+    
   
 })(jQuery);
