6a1100b42d5a6913662bb2e0c9e6b0670aad8195
Kim Nguyen <zktnguyen@ucdavis.edu>
2017-07-07 00:54:24 -0700

    clean up, most logic. To do: change format of the timer to HH:MM:SS, Have a caption for the timer, change colors when the break timer is over.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,94 @@
 (function($) {
   $(function(){
     var toggle = $("#toggle-timer"),
-      timer = $("#clock-timer"),
       breakTime = $("#break-time"),
-      length = $("#timer-length")
+      length = $("#timer-length"),
       decLength = $("#dec-length"),
       incLength = $("#inc-length"),
       decBreak = $("#dec-break"),
-      incBreak = $("#inc-break");
-    var timeRemaining = parseFloat(length.html()),
+      incBreak = $("#inc-break"),
+      clockTimer = $("#clock-timer");
+    var timeLength = parseInt(length.html()),
       minimumLength = 1,
-      breakLength = parseFloat(breakTime.html()),
-      timerInSeconds = timeRemaining * 60,
-      timerType = "S", // "S" = regular timer, "B" = break timer
-      timerOn = false;
-
-    function beginTimer() {
-
-    }
-
-    function stopTimer() {
-
-    }
-
-
-    toggle.on("click",function(e){
-      if (!timerOn){
-        beginTimer();
+      breakLength = parseInt(breakTime.html()),
+      timerInSeconds = timeLength * 60,
+      type = true, // true = session timer, false = break timer
+      timerOn = false,
+      displayTime = '',
+      buzzer = new Audio('');
+    
+    decLength.on("click", function(){
+      if (timeLength > 5) {
+        timeLength -= 5;
       }
       else {
-        stopTimer();
+        timeLength = 5;
+      }
+      timerInSeconds = timeLength * 60;
+      length.html(timeLength);
+    });
+
+    incLength.on("click", function(){
+      if (timeLength < 60) {
+        timeLength += 5;
+      }
+      else {
+        timeLength = 60;
+      }
+      timerInSeconds = timeLength * 60;
+      length.html(timeLength);
+    });
+
+    decBreak.on("click", function(){
+      if (breakLength > 1){
+        breakLength--;
+      }
+      else {
+        breakLength = 1;
+      }
+      breakTime.html(breakLength);
+    });
+
+    incBreak.on("click", function(){
+      if (breakLength < 20){
+        breakLength++;
+      }
+      else {
+        breakLength = 20;
+      }
+      breakTime.html(breakLength);
+    });
+
+    function changeBackground() {
+      $("body").css('background-color', '#a2f9b3');
+      $(".clock-design").css("border", "10px dashed #a2f9b3");
+    }
+
+    function playBreak() {
+      changeBackground();
+      timerInSeconds = breakLength * 60;
+      var breakClock = setInterval(timer, 1000);
+      function timer() {
+        timerInSeconds -= 1;
+        if (timerInSeconds <= 0){
+          buzzer.play();
+          clearInterval(breakClock);
+
+        }
+        clockTimer.html(timerInSeconds);
+      }
+    }
+
+    toggle.on("click",function(){
+      var clock = setInterval(timer, 1000);
+      function timer() {
+        timerInSeconds -= 1;
+        if (timerInSeconds <= 0){
+          buzzer.play();
+          clearInterval(clock);
+          playBreak();
+        }
+        clockTimer.html(timerInSeconds);
       }
     });
   });
